10a63bb7c1210b9316adbb46594100843b8280ec
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-09-05 19:03:39 -0400

    added a description input to enter income

--- src/inco.js
+++ src/inco.js
@@ -1,10 +1,12 @@
 
 const amou = document.querySelector("#amount");
+const desc = document.querySelector("#description");
 const form = document.querySelector("#income-form");
 const div = document.querySelector("#income-div");
 
 form.addEventListener("submit", (event)=>{
    event.preventDefault();
    const amount= Number.parseInt(amou.value);
-   div.innerHTML = "<p>"+ amount +"<p/>"
+   const description= desc.value;
+   div.innerHTML = "<p>"+` Added income: ${amount} ` + `Description: ${description}` +"<p/>"
 });
