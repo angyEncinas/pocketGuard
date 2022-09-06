563a2d376e610c70d6543d3b877122f0e3d797d0
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-09-05 22:08:09 -0400

    Show total expenses

--- src/expens.js
+++ src/expens.js
@@ -2,8 +2,8 @@
 const exp = document.querySelector("#expense");
 const desc = document.querySelector("#descrip");
 const form = document.querySelector("#expense-form");
-const div = document.querySelector("#expense-div");
 const divhistorial = document.querySelector("#historial-div");
+const divsuma = document.querySelector("#expense-div");
 let expens = [];
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
@@ -11,11 +11,16 @@ form.addEventListener("submit", (event)=>{
     const description= desc.value;
     expens.push([amount,description]);
     let txt = "";
+    let sumae = 0;
     expens.forEach(myFunction);
     divhistorial.innerHTML = txt;
-
+    expens.forEach(suma);
+    divsuma.innerHTML = "Total expensas:" + sumae;
     function myFunction(value) {
         txt += value + "<br>"; 
     }
-     
+    function suma(value) {
+        sumae += value[0]; 
+    }
+    
  });
