b0a8bafec20a625eb5aef26dbe14d73ab0ecbef5
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-09-05 21:42:25 -0400

    Show expenses history

--- src/expens.js
+++ src/expens.js
@@ -3,10 +3,22 @@ const exp = document.querySelector("#expense");
 const desc = document.querySelector("#descrip");
 const form = document.querySelector("#expense-form");
 const div = document.querySelector("#expense-div");
-
+const divhistorial = document.querySelector("#historial-div");
+let expens = [];
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
     const amount= Number.parseInt(exp.value);
     const description= desc.value;
-    div.innerHTML = "<p>"+` Added Expense: ${amount} `+ `Description: ${description}` +"<p/>"
+    expens.push([amount,description]);
+    div.innerHTML = "<p>"+` Added Expense: ${expens[0]} `+ "<p/>"
+    const numbers = [45, 4, 9, 16, 25];
+
+    let txt = "";
+    expens.forEach(myFunction);
+    divhistorial.innerHTML = txt;
+
+    function myFunction(value) {
+        txt += value + "<br>"; 
+    }
+     
  });
