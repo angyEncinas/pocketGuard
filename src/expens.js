a43279415ad7020a60e409df6914cf0eaf051c62
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-09-05 20:06:21 -0400

    added function to add expenses description

--- src/expens.js
+++ src/expens.js
@@ -1,10 +1,12 @@
 
 const exp = document.querySelector("#expense");
+const desc = document.querySelector("#descrip");
 const form = document.querySelector("#expense-form");
 const div = document.querySelector("#expense-div");
 
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
     const amount= Number.parseInt(exp.value);
-    div.innerHTML = "<p>"+` Added Expense: ${amount} ` +"<p/>"
+    const description= desc.value;
+    div.innerHTML = "<p>"+` Added Expense: ${amount} `+ `Description: ${description}` +"<p/>"
  });
